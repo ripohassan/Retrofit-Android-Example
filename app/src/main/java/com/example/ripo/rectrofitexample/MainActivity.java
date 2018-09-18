ced0048c1c1724ae1918086e36272e333c343229
ripo <ripohassan5@gmail.com>
2018-09-18 11:56:46 +0600

    init commit

---
+++ app/src/main/java/com/example/ripo/rectrofitexample/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.ripo.rectrofitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //listView = (ListView) findViewById(R.id.listViewHeroes);
+
+        recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //calling the method to display the heroes
+        getHeroes();
+    }
+
+    private void getHeroes() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()) //Here we are using the GsonConverterFactory to directly convert json data to object
+                .build();
+
+        Api api = retrofit.create(Api.class);
+
+        Call<List<Hero>> call = api.getHeroes();
+
+        call.enqueue(new Callback<List<Hero>>() {
+            @Override
+            public void onResponse(Call<List<Hero>> call, Response<List<Hero>> response) {
+                List<Hero> heroList = response.body();
+
+                //Creating an String array for the ListView
+                String[] heroes = new String[heroList.size()];
+
+                //looping through all the heroes and inserting the names inside the string array
+                for (int i = 0; i < heroList.size(); i++) {
+                    heroes[i] = heroList.get(i).getName();
+                }
+
+
+                //displaying the string array into listview
+               // listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, heroes));
+                recyclerView.setAdapter(new MoviesAdapter(heroList, R.layout.list_item_movie, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<List<Hero>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/ripo/rectrofitexample/MoviesAdapter.java
@@ -0,0 +1,65 @@
+package com.example.ripo.rectrofitexample;
+
+import android.content.Context;
+import android.graphics.Movie;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    private List<Hero> movies;
+    private int rowLayout;
+    private Context context;
+
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout moviesLayout;
+        TextView movieTitle;
+        TextView data;
+        TextView movieDescription;
+        TextView rating;
+
+
+        public MovieViewHolder(View v) {
+            super(v);
+            moviesLayout = (LinearLayout) v.findViewById(R.id.movies_layout);
+            movieTitle = (TextView) v.findViewById(R.id.title);
+            data = (TextView) v.findViewById(R.id.subtitle);
+            movieDescription = (TextView) v.findViewById(R.id.description);
+            rating = (TextView) v.findViewById(R.id.rating);
+        }
+    }
+
+    public MoviesAdapter(List<Hero> movies, int rowLayout, Context context) {
+        this.movies = movies;
+        this.rowLayout = rowLayout;
+        this.context = context;
+    }
+
+    @Override
+    public MoviesAdapter.MovieViewHolder onCreateViewHolder(ViewGroup parent,
+                                                            int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, final int position) {
+        holder.movieTitle.setText(movies.get(position).getName());
+        holder.data.setText(movies.get(position).getPublisher());
+        holder.movieDescription.setText(movies.get(position).getCreatedby());
+        holder.rating.setText(movies.get(position).getTeam().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+}
